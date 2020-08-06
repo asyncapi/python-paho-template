73dda26bf7eb3d9642f15a535bfaf6272b316fcc
Michael Davis <michael@damaru.com>
2020-08-05 20:08:09 -0400

    fixes #4 This template now works when the payload schema is embedded inside an operation, as is the case with the Streetlights demo.

--- hooks/post-process.js
+++ hooks/post-process.js
@@ -5,13 +5,24 @@ const _ = require('lodash');
 module.exports = {
   'generate:after': generator => {
     const asyncapi = generator.asyncapi;
+    let hasSchema = false;
 
-    for (schema in asyncapi.components().schemas()) {
-      let oldName = schema + ".py";
-      let newName = _.lowerFirst(oldName);
-      if (newName !== schema) {
-        fs.renameSync(path.resolve(generator.targetDir, oldName), path.resolve(generator.targetDir, newName));
+    if (asyncapi.components()) {
+
+      for (schema in asyncapi.components().schemas()) {
+        hasSchema = true;
+        let oldName = schema + ".py";
+        let newName = _.lowerFirst(oldName);
+        if (newName !== schema) {
+          fs.renameSync(path.resolve(generator.targetDir, oldName), path.resolve(generator.targetDir, newName));
+        }
       }
+    }
+
+    // If there are no schemas, we expect to find an anonymous one embedded in a payload. If we do have schemas we assume we don't need this.
+    // This will turn out to be a bug if we ever have a file with schemas, but which also has an anonymous schema embedded in an operation.
+    if (hasSchema) {
+      fs.unlinkSync(path.resolve(generator.targetDir, 'payload.py'));
     }
   }
 }
--- template/messaging.py
+++ template/messaging.py
@@ -10,6 +10,8 @@ class Messaging:
     def __init__(self, config, subscription = None, on_message = None, clientId = None):
         global on_connect
         self.config = config
+        defaultHost = '{{ asyncapi | server }}'
+        logging.info("Default host:", defaultHost)
 
         if (clientId):
             self.client = mqtt.Client(clientId)
@@ -25,9 +27,20 @@ class Messaging:
         if (on_message):
             self.client.on_message = on_message
 
-        self.client.username_pw_set(config['username'], config['password'])
-        port = int(config['port'])
-        self.client.connect(config['host'], port)
+        username = config.get('username', None)
+        password = config.get('password', None)
+
+        if username is not None:
+            self.client.username_pw_set(username, password)
+            
+        port = int(config.get('port', '1883'))
+        host = config.get('host', defaultHost)
+        print("Host: ", host, "port: ", port)
+
+        if host is None:
+            raise Exception("Host must be defined in the config file or in the servers section.")
+
+        self.client.connect(host, port)
 
     def publish(self, topic, payload, qos = 0, retain = False):
         self.client.publish(topic, payload, qos, retain)
